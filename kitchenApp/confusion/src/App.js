718e208488eb2e5c920a622ea7c6e66e47d85d18
tanusgit <24976766+tanusgit@users.noreply.github.com>
2020-05-10 14:04:38 -0700

    componenet part 1

---
+++ kitchenApp/confusion/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponent';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">
+          Ristorente Con Fusion
+          </NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
