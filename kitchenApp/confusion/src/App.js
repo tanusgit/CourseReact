551504bb14ef2f78489e9d1f16ddcd7509b957b1
tanusgit <24976766+tanusgit@users.noreply.github.com>
2020-05-10 15:00:09 -0700

    componenet part2

--- kitchenApp/confusion/src/App.js
+++ kitchenApp/confusion/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 
-function App() {
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      dishes: DISHES
+    };
+  }
+  render(){
   return (
     <div>
       <Navbar dark color="primary">
@@ -15,9 +24,10 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
